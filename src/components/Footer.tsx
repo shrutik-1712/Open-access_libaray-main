0fb923c894e779cb0b997bff7467366767f5e617
shrutik <shrutikdawane@gmail.com>
2024-10-09 17:16:43 +0530

    changed header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
 import { MapPin, Phone, Facebook, Twitter, Instagram, Linkedin } from 'lucide-react';
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { MapPin, Phone, Menu, X } from 'lucide-react';
-import { Link, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
